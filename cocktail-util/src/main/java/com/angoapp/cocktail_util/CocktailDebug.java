2fc65da92b345da1fcb6b31f40f768bc65869291
Jose Francisco <junior.poster@gmail.com>
2018-09-30 04:17:23 -0400

    Added static methods for testing

--- cocktail-util/src/main/java/com/angoapp/cocktail_util/CocktailDebug.java
+++ cocktail-util/src/main/java/com/angoapp/cocktail_util/CocktailDebug.java
@@ -3,9 +3,9 @@ package com.angoapp.cocktail_util;
 import android.util.Log;
 
 public class CocktailDebug {
-    public final String TAG = "Cocktail Library";
+    public static final String TAG = "Cocktail Library";
 
-    public void d(String msg){
+    public static void d(String msg){
         Log.d(TAG, msg);
     }
 }
