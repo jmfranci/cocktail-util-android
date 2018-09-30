b4d699cbaa207a2cae31fed1bcf5c2298efcda8d
Jose Francisco <junior.poster@gmail.com>
2018-09-30 04:14:22 -0400

    Added sample function

--- cocktail-util/src/main/java/com/angoapp/cocktail_util/CocktailDebug.java
+++ cocktail-util/src/main/java/com/angoapp/cocktail_util/CocktailDebug.java
@@ -3,5 +3,9 @@ package com.angoapp.cocktail_util;
 import android.util.Log;
 
 public class CocktailDebug {
+    public final String TAG = "Cocktail Library";
 
+    public void d(String msg){
+        Log.d(TAG, msg);
+    }
 }
